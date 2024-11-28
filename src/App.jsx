e10f862579054d69fc7bef15334f5b7360282c41
Danil L <danillitvinenkod00@gmail.com>
2024-11-28 15:27:19 +0300

    Implement the main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+
+const Content = styled.div`
+  padding: 120px 0;
+`;
+
+const H2 = styled.h2`
+  text-align: center;
+`;
+
+const Header = () => <div>Шапка сайта</div>;
+const Footer = () => <div>Подвал сайта</div>;
+
 function App() {
   return (
     <>
-      <i className="fa fa-camera-retro fa-lg"></i>
+      <Header />
+      <Content>
+        <H2>Контент Страницы</H2>
+
+        <Routes>
+          <Route path="/" element={<div>Главная страница</div>} />
+          <Route path="/login" element={<div>Страница авторизации</div>} />
+          <Route path="/register" element={<div>Страница регистрации</div>} />
+          <Route path="/users" element={<div>Страница пользователей</div>} />
+          <Route path="/post" element={<div>Страница новой статьи</div>} />
+          <Route path="/post/:postId" element={<div>Страница статьи</div>} />
+          <Route path="*" element={<div>Страница not found</div>} />
+        </Routes>
+      </Content>
+      <Footer />
     </>
   );
 }
